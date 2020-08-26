88e9c11bed4dcf8074f8b44e6521939116261e7c
Ricardo Gusi <ricardogusi@hotmail.com>
2020-08-26 11:25:30 -0300

    criação das página e estilização

--- src/Components/Estoque/Estoque.jsx
+++ src/Components/Estoque/Estoque.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
+import React from "react";
+import CardPrincipal from "../CardPrincipal/CardPrincipal";
+import styles from './estoque.module.css'
+import {ReactComponent as Pencil} from '../../Assets/pencil.svg'
+
 
 const Estoque = () => {
-    return (
-        <div className="container">
+  return (
+    <CardPrincipal>
+       <div className='header'>
+        <p>Estoque</p>
+        <button>+</button>{" "}
+      </div>
+      <div className='container' >
+        <div>
+          <ul  className={styles.produtoHead}>
+            <li>Código</li>
+            <li>Nome</li>
+            <li>Quantidade</li>
+            <li>Preço Unitário</li>
+          </ul>
+          <ul  className={styles.produto}>
+            <li>354</li>
+            <li>Pote de Mel 500gr</li>
+            <li>250</li>
+            <li>R$ 50,00</li>
+            <Pencil className={styles.pencil} />            
+          </ul>
+          <ul  className={styles.produto}>
+            <li>354</li>
+            <li>Pote de Mel 500gr</li>
+            <li>250</li>
+            <li>R$ 50,00</li>
+            <Pencil className={styles.pencil} />            
+          </ul>
+          <ul  className={styles.produto}>
+            <li>354</li>
+            <li>Pote de Mel 500gr</li>
+            <li>250</li>
+            <li>R$ 50,00</li>
+            <Pencil className={styles.pencil} />            
+          </ul>
+          <ul  className={styles.produto}>
+            <li>354</li>
+            <li>Pote de Mel 500gr</li>
+            <li>250</li>
+            <li>R$ 50,00</li>
+            <Pencil className={styles.pencil} />            
+          </ul>
+          </div>
+      </div>
+    </CardPrincipal>
+  );
+};
 
-            estoque
-        </div>
-    )
-}
-
-export default Estoque
+export default Estoque;
--- src/Components/Vendas/Vendas.jsx
+++ src/Components/Vendas/Vendas.jsx
@@ -1,19 +1,59 @@
 import React from "react";
-import styles from "./vendas.module.css";
+
 import CardPrincipal from "../CardPrincipal/CardPrincipal";
 
 const Vendas = () => {
   return (
     <CardPrincipal>
-      <div className={styles.header}>
+      <div className='header'>
         <p>Histórico de vendas</p>
         <button>+</button>{" "}
       </div>
-      <div className={styles.container} >
-        <p>Nome </p>
-        <p>Data</p>
-        <p>Total</p>
-        <p>Informações</p>
+      <div className='container' >
+        <div>
+          <ul  className='barraHead'>
+            <li>Nome</li>
+            <li>Data</li>
+            <li>Total</li>
+            <li>Info</li>
+          </ul>
+          <ul  className='barra'>
+            <li>Ricardo Gusi</li>
+            <li>26/08/2020</li>
+            <li>R$ 500,00</li>
+            <li><button>Mais informações</button></li>
+          </ul>
+          <ul  className='barra'>
+            <li>Ricardo Gusi</li>
+            <li>26/08/2020</li>
+            <li>R$ 500,00</li>
+            <li><button>Mais informações</button></li>
+          </ul>
+          <ul  className='barra'>
+            <li>Ricardo Gusi</li>
+            <li>26/08/2020</li>
+            <li>R$ 500,00</li>
+            <li><button>Mais informações</button></li>
+          </ul>
+          <ul  className='barra'>
+            <li>Ricardo Gusi</li>
+            <li>26/08/2020</li>
+            <li>R$ 500,00</li>
+            <li><button>Mais informações</button></li>
+          </ul>
+          <ul  className='barra'>
+            <li>Ricardo Gusi</li>
+            <li>26/08/2020</li>
+            <li>R$ 500,00</li>
+            <li><button>Mais informações</button></li>
+          </ul>
+          <ul  className='barra'>
+            <li>Ricardo Gusi</li>
+            <li>26/08/2020</li>
+            <li>R$ 500,00</li>
+            <li><button>Mais informações</button></li>
+          </ul>
+        </div>
 
         
        </div>
